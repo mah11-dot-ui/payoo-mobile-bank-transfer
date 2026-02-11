a43882fa8aed6e0a466b6b90ce5459c270067811
mah11-dot-ui <mhassanwbdpr++++@gmail.com>
2026-02-11 15:19:57 -0800

    changed home.js and home.html

--- js/home.js
+++ js/home.js
@@ -1,1 +1,10 @@
-console.log("home added");
+document.getElementById('btn-add-money').addEventListener('click', function(e){
+    e.preventDefault();
+    const AddMoneyInput = document.getElementById('input-add-money').value;
+    console.log('Amount:' + AddMoneyInput);
+
+    const PinNumberInput = document.getElementById('pin-number').value;
+    console.log('Pin Number:' + PinNumberInput);
+    
+    
+})
