669aa98d098f566253d1440dbb716f5d427cb791
mah11-dot-ui <mhassanwbdpr++++@gmail.com>
2026-02-14 07:48:19 -0800

    changed home.js and home.html

--- js/home.js
+++ js/home.js
@@ -4,7 +4,24 @@ document.getElementById('btn-add-money').addEventListener('click', function(e){
     console.log('Amount:' + AddMoneyInput);
 
     const PinNumberInput = document.getElementById('pin-number').value;
-    console.log('Pin Number:' + PinNumberInput);
+    console.log('Pin Number:' + typeof PinNumberInput);
     
-    
+
+    if(PinNumberInput === '1234'){
+        console.log("added money to your acount");
+
+        const balance = document.getElementById('balance').innerHTML;
+        console.log("Balnce:" + typeof balance);
+        
+        const addmoneyNumber = parseFloat(AddMoneyInput);
+        const balanceNumber = parseFloat(balance);
+        const newBalnce = addmoneyNumber + balanceNumber;
+        console.log("New Balance:" + newBalnce);
+
+        document.getElementById('balance').innerHTML = newBalnce;
+        
+        
+    }else{
+        alert("Wrong Pin Number");
+    }
 })
